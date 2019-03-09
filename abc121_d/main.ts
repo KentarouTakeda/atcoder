5c8eabe6fa2bd02391efd937047ad87ca82f8073
武田 憲太郎 <takeda@youmind.jp>
2019-03-09 23:51:41 +0900

    fix TLE

--- abc121_d/main.ts
+++ abc121_d/main.ts
@@ -6,12 +6,22 @@ export function main(strings: string[]): string {
 	const numbers = strings.map(Number);
 
 	const [A, B] = numbers;
-
-	let answer = A
-	for(let i=A+1; i<=B; i++) {
-		answer = answer ^ i;
-	}
+	const answer = f(A-1) ^ f(B);
 
 	ret.push(answer);
 	return ret.join('\n') + '\n';
+}
+
+function f(n: number): number {
+	switch(n%4) {
+	case 0:
+		return n;
+	case 1:
+		return 1;
+	case 2:
+		return n+1;
+	case 3:
+		return 0;
+	}
+	throw new Error();
 }
