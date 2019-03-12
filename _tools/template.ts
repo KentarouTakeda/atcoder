3373edeed769b812e936c96b25199f666e888cb2
武田 憲太郎 <takeda@youmind.jp>
2019-03-12 19:12:22 +0900

    多言語対応していない出題ページのスクレイピングに対応

--- _tools/template.ts
+++ _tools/template.ts
@@ -6,7 +6,13 @@ const url = 'https://atcoder.jp/contests/agc030/tasks/agc030_a';
 
 const dom = JSDOM.fromURL(url).then(dom => {
 	const document = dom.window.document;
-	const pres = document.querySelectorAll<HTMLPreElement>('[class="lang-ja"] pre');
+	const lang = document.getElementsByClassName('lang-ja');
+	let search: (Element|Document) = document;
+	if(lang.length) {
+		search = lang[0];
+	}
+
+	const pres = search.getElementsByTagName('pre');
 	if(pres.length === 0) {
 		throw new Error();
 	}
